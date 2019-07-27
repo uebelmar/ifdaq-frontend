ab4006b1bc5fb9631013c6102766f3800a0e54d7
Markus Übeleis <office@cope-it.at>
2019-07-27 21:19:16 +0200

    updated to new server response format

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,58 +1,61 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'environments/environment';
-import { map, tap } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from 'environments/environment';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  apiUrl = environment.apiUrl;
+    apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  public login({username, password}): Observable<any> {
-    const authorizationData = 'Basic ' + btoa(username + ':' + password);
+    public login({username, password}): Observable<any> {
+        const authorizationData = 'Basic ' + btoa(username + ':' + password);
 
-    const headersOptions = new HttpHeaders({
-            'Content-Type':  'application/json',
+        const headersOptions = new HttpHeaders({
+            'Content-Type': 'application/json',
             'Authorization': authorizationData
         });
-    return this.http.get<LoginSuccess>(`${this.apiUrl}login`, { headers: headersOptions })
-      .pipe(
-        tap((response: LoginSuccess) => {
-          console.log(response);
-          const token = response.token;
-          if (token) {
-            localStorage.setItem('token', token);
-            const user = {
-              username: response.username,
-              email: response.email
-            };
-            localStorage.setItem('user', JSON.stringify(user));
-          }
-        })
-      );
-  }
+        return this.http.get<LoginSuccess>(`${this.apiUrl}login`, {headers: headersOptions})
+            .pipe(
+                tap((response: LoginSuccess) => {
+                    console.log(response);
+                    const token = response.data.token;
+                    if (token) {
+                        localStorage.setItem('token', token);
+                        const user = {
+                            username: response.data.username,
+                            email: response.data.email
+                        };
+                        localStorage.setItem('user', JSON.stringify(user));
+                    }
+                })
+            );
+    }
 
-  public logout(): Observable<any> {
-    return this.http.get(`${this.apiUrl}logout`)
-      .pipe(
-        tap((response) => {
-          console.log(response);
-          localStorage.removeItem('token');
-          localStorage.removeItem('user');
-        })
-      );
-  }
+    public logout(): Observable<any> {
+        return this.http.get(`${this.apiUrl}logout`)
+            .pipe(
+                tap((response) => {
+                    console.log(response);
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('user');
+                })
+            );
+    }
 }
 
 interface LoginSuccess {
-  username: string;
-  email: string;
-  token: string;
-  type: any;
-  success: true;
+    data: {
+        username: string;
+        email: string;
+        token: string;
+        type: any;
+    },
+    success: true;
 }
