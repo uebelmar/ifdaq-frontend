1a40e35de0f0596b2330c9e64f65b0eee0c96f4b
Markus Übeleis <office@cope-it.at>
2019-08-16 12:54:24 +0200

    adding some real datas

--- src/app/main/dashboard/components/line-basic/line-basic.component.ts
+++ src/app/main/dashboard/components/line-basic/line-basic.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import {Component, OnInit, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
 import * as Highcharts from 'highcharts';
+import {DashboardService} from '../../services/dashboard.service';
 
 declare var require: any;
 const boost = require('highcharts/modules/boost');
@@ -13,86 +14,63 @@ noData(Highcharts);
 
 
 @Component({
-  selector: 'app-line-basic',
-  templateUrl: './line-basic.component.html',
-  styleUrls: ['./line-basic.component.scss']
+    selector: 'app-line-basic',
+    templateUrl: './line-basic.component.html',
+    styleUrls: ['./line-basic.component.scss']
 })
 export class LineBasicComponent implements AfterViewInit {
-  
-  @ViewChild('lineBasic', {static: false}) container: ElementRef;
+    @ViewChild('lineBasic', {static: false}) container: ElementRef;
+    datas: Array<object>;
 
-  options: Highcharts.Options = {
-    title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
-    },
-    subtitle: {
-        text: 'Source: thesolarfoundation.com'
-    },
-    yAxis: {
-        title: {
-            text: 'Number of Employees'
-        }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
+    getOptions(): object {
+
+        return {
+
+            chart: {
+                type: 'line'
             },
-            pointStart: 2010
-        }
-    },
-    series: 
-    [
-      {
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-        type: 'line'
-      },
-      {
-          name: 'Manufacturing',
-          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-          type: 'line'
-      },
-      {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-        type: 'line'
-      },
-      {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-        type: 'line'
-      },
-      {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-        type: 'line'
-      }
-    ],
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
+            title: {
+                text: 'Top 5 Models'
             },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
+            xAxis: {
+                categories: this.datas['dates']
+            },
+            yAxis: {
+                title: {
+                    text: 'Points'
                 }
-            }
-        }]
-    }
-  };
-  constructor() { }
+            },
+            plotOptions: {
+                line: {
+                    marker: {
+                        enabled: false
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    enableMouseTracking: true
+                }
+            },
+            series: this.datas['top5ModelsHistory'],
 
-  ngAfterViewInit(): void {
-    Highcharts.chart(this.container.nativeElement, this.options);
-  }
+        };
+    }
+
+    constructor(private dashboardService: DashboardService) {
+    }
+
+
+    getDashboardData(): void {
+        this.dashboardService.getDashboardData()
+            .subscribe((data) => {
+                this.datas = data['models']['chart'];
+                Highcharts.chart(this.container.nativeElement, this.getOptions());
+
+            });
+    }
+
+    ngAfterViewInit(): void {
+        this.getDashboardData();
+    }
 
 }
--- src/app/main/dashboard/services/dashboard.service.ts
+++ src/app/main/dashboard/services/dashboard.service.ts
@@ -11,25 +11,6 @@ export class DashboardService {
 
   apiUrl = environment.apiUrl;
 
-  data = {
-    loosers: [
-      {
-        first_name: 'adsgadg addssg',
-        last_name: 'adsgadg addssg',
-        ifdaq_sum_value: '486.34',
-        ifdaq_sum_value_yesterday: '526.34',
-        rank: '1252',
-      },
-      {
-        first_name: 'a34dg addssg',
-        last_name: 'ads3dfhdg addssg',
-        ifdaq_sum_value: '286.34',
-        ifdaq_sum_value_yesterday: '26.34',
-        rank: '22',
-      },
-    ]
-  };
-  
   constructor(
     private http: HttpClient
   ) { }
@@ -37,11 +18,8 @@ export class DashboardService {
   public getDashboardData(): Observable<object> {
     return this.http.get(`${this.apiUrl}dashboard`)
       .pipe(
-          tap( (response) => {
-            console.log(response);
-          }),
-          map((response: {data: {models: {}}}) => {
-            return response.data.models;
+          map((response: {data: {models: {}, chart: {}}}) => {
+            return response.data;
           })
       );
   }
